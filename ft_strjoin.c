d28b959b6823049150dd9d490598a873f5bb3f9e
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-03 13:41:32 +0100

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 16:37:57 by rgobet            #+#    #+#             */
+/*   Updated: 2023/11/02 16:51:11 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_strlen1(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*tab;
+	int		i;
+	int		j;
+
+	tab = malloc((1 + ft_strlen1(s1) + ft_strlen1(s2)) * sizeof(char));
+	i = 0;
+	j = 0;
+	if (tab == 0)
+		return (0);
+	while (s1[i])
+	{
+		tab[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		tab[i] = s2[j];
+		j++;
+		i++;
+	}
+	return (tab);
+}
