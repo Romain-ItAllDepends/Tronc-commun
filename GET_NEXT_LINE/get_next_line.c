3c7ded853f6976aee60492445be6e5e66984b27c
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-15 14:59:12 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line.c
+++ GET_NEXT_LINE/get_next_line.c
@@ -6,11 +6,25 @@
 /*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:32:11 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/15 12:05:09 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/11/15 14:58:11 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_bn(char *s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*ft_swap(char *buffer, char *stash)
 {
@@ -22,6 +36,7 @@ char	*ft_swap(char *buffer, char *stash)
 	{
 		stash[i] = buffer[j];
 		i++;
+		j++;
 	}
 	return (stash);
 }
@@ -29,36 +44,75 @@ char	*ft_swap(char *buffer, char *stash)
 int	ft_read(int fd, char *buffer)
 {
 	if (read (fd, buffer, BUFFER_SIZE) < 0)
+		return (1);
+	return (0);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	static char		*ptr;
+	char			*tab;
+	long int		size_m;
+
+	tab = ptr;
+	ptr = &tab[0];
+	if (size == 0)
+		size = BUFFER_SIZE;
+	size_m = nmemb * size + 2;
+	if ((int)size_m < 0 || ((int)size < 0 && (int)nmemb < 0))
+		return (NULL);
+	tab = malloc(size_m);
+	if (tab == 0)
 		return (0);
-	return (n);
+	tab[size_m - 1] = '\0';
+	return (tab);
 }
 
 char	*get_next_line(int fd)
 {
 	int			i;
-	int			length;
+	int			buff;
 	char		*line;
-	char		*buffer[BUFFER_SIZE];
-	static char	*stash;
+	char		buffer[BUFFER_SIZE];
+	char	*stash;
 
 	i = 0;
-	while (stash[i] != '\n')
+	buff = BUFFER_SIZE;
+	if (ft_read(fd, buffer))
+		return (0);
+	while (i == 0 || stash[i] != '\n')
 	{
-		if (0 == (BUFFER_SIZE % i))
+		if (i == 0 || (buff / i) == 0)
 		{
-			ft_malloc(stash, i);
+			stash = ft_calloc(sizeof(char), i);
 			ft_swap(buffer, stash);
+			if (!ft_bn(stash))
+				free(stash);
 		}
-		if (stash != '\n')
-			free(stash);
 		i++;
 	}
-	stash[i] = '\0';
-	line = malloc((ft_strlen(stash) + 1) * sizeof(char));
-	if (line == 0)
-		return (0);
-	line = ft_substr(stash, 0, ft_strlen(stash));
-	length = ft_moveptr(stash);
-	stash = &stash[ft_moveptr(stash)];
+	line = ft_substr(stash, 0, ft_strlen_mod(stash));
+	free(stash);
 	return (line);
+}
+
+#include <stdio.h>
+
+int    main(void)
+{
+    int        fd;
+    int        i;
+    char    *rsl;
+
+    i = 0;
+    fd = open("files/empty", O_RDONLY);
+    while (i < 3)
+    {
+        rsl = get_next_line(fd);
+        printf("%s", rsl);
+        i++;
+        free(rsl);
+    }
+    close(fd);
+    return (0);
 }
