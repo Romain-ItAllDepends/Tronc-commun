9605cf6469c6c5b536a587527eadee2d0fdd618f
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-16 15:56:20 +0100

    Add files via upload

--- FT_PRINTF/ft_printf.h
+++ FT_PRINTF/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:01:38 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/16 14:21:53 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/11/16 15:54:26 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdarg.h>
 # include "LIBFT/libft.h"
 
-
-int ft_printf(const char *s, ...);
+int	ft_printf(const char *s, ...);
+int	ft_type(char s, va_list print);
 
 #endif
