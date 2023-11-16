b3f06434c7345c8b1f915326f18b1670b1b86c19
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-16 15:55:58 +0100

    Add files via upload

--- FT_PRINTF/ft_printf.c
+++ FT_PRINTF/ft_printf.c
@@ -6,23 +6,39 @@
 /*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:08:36 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/16 15:13:07 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/11/16 15:54:23 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
+
+int	ft_type(char s, va_list print)
+{
+	if (s == 's')
+		return (ft_putstr_fd(va_arg(print, char *), 0));
+	else if (s == 'c')
+		return (ft_putchar_fd(va_arg(print, int), 0));
+	else if (s == 'd')
+		return (ft_putnbr_fd(va_arg(print, int), 0));
+	else
+		return (0);
+}
 
 int ft_printf(const char *s, ...)
 {
 	va_list print;
+	int		result;
 	int		i;
 
 	i = 0;
+	result = 0;
 	va_start(print, s);
 	while (s[i])
 	{
-		printf("%s ", va_arg(print, char *));
+		if (s[i] == '%' && s[i] != '\0')
+			result += ft_type(s[i], print);
+		else
+			ft_putchar_fd(s[i], 0);
 		i++;
 	}
 	va_end(print);
@@ -33,6 +49,6 @@ int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	ft_printf("Star", "J'aime", "les", "kinder", "bueno");
+	ft_printf("Chiffre = %d,\nLettre = %c\nString = %s\n", av[1], av[2], av[3]);
 	return (0);
 }
