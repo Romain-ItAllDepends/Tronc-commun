c3dbf592085fd5b5008790f3874706f2b39f4b47
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-17 15:51:59 +0100

    Add files via upload

--- FT_PRINTF/ft_printf.c
+++ FT_PRINTF/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:08:36 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/16 15:54:23 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/11/17 14:04:23 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,23 @@
 int	ft_type(char s, va_list print)
 {
 	if (s == 's')
-		return (ft_putstr_fd(va_arg(print, char *), 0));
+		return (ft_putstr_fd(va_arg(print, char *), 1));
 	else if (s == 'c')
-		return (ft_putchar_fd(va_arg(print, int), 0));
+		return (ft_putchar_fd(va_arg(print, int), 1));
 	else if (s == 'd')
-		return (ft_putnbr_fd(va_arg(print, int), 0));
+		return (ft_putnbr_fd(va_arg(print, int), 1));
+	else if (s == 'i')
+		return (ft_putnbr_fd(va_arg(print, int), 1));
+	else if (s == 'u')
+		return (ft_putnbru_fd(va_arg(print, int), 1));
+	else if (s == 'x')
+		return (ft_puthex_fd(va_arg(print, int), 1, "0123456789abcdef"));
+	else if (s == 'X')
+		return (ft_puthex_fd(va_arg(print, int), 1, "0123456789ABCDEF"));
+	else if (s == 'p')
+		return (ft_putptr_fd(va_arg(print, unsigned long long), 1, "0123456789abcdef"));
+	else if (s == '%')
+		return (ft_putchar_fd('%', 1));
 	else
 		return (0);
 }
@@ -32,23 +44,37 @@ int ft_printf(const char *s, ...)
 
 	i = 0;
 	result = 0;
-	va_start(print, s);
+	va_start (print, s);
 	while (s[i])
 	{
-		if (s[i] == '%' && s[i] != '\0')
+		if (s[i] == '%' && s[i + 1] != '\0')
+		{
+			i++;
 			result += ft_type(s[i], print);
+		}
 		else
-			ft_putchar_fd(s[i], 0);
+		{
+			write(1, &s[i], 1);
+			result++;
+		}
 		i++;
 	}
 	va_end(print);
-	return (0);
+	return (result);
 }
+/*
+#include <stdio.h>
+#include <limits.h>
 
 int	main(int ac, char **av)
 {
-	(void)ac;
 	(void)av;
-	ft_printf("Chiffre = %d,\nLettre = %c\nString = %s\n", av[1], av[2], av[3]);
+	if (ac == 0)
+		return (1);
+	else
+		ft_printf(" %p %p ", LONG_MIN, LONG_MAX);
+	ft_printf(" %p %p ", ULONG_MAX, -ULONG_MAX);
+	printf(" %p %p ", ULONG_MAX, -ULONG_MAX);
+	printf(" %p %p ", LONG_MIN, LONG_MAX);
 	return (0);
-}
+}*/
