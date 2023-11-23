4532cc4e4fb5d62211ee3c392198e90ad236be5d
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-23 17:01:50 +0100

    Add files via upload

---
+++ GET_NEXT_LINE/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 12:36:05 by rgobet            #+#    #+#             */
+/*   Updated: 2023/11/23 16:46:44 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+#endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_split(char *line, char *buffer, int count);
+int	ft_read(int fd, char *s);
+int		ft_back(char *s);
+int		ft_strlen(char const *s);
+
+#endif
