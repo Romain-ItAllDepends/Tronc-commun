98f676ad1084044a2549d9cd6d6ac4f40538ead7
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-11-27 14:24:10 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line.c
+++ GET_NEXT_LINE/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:34:36 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/24 16:23:17 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/11/25 10:50:58 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ char *get_next_line(int fd)
 	while (1)
 	{
 		if (ft_back(line) > 0)
-			if (line != NULL)
-				return (line);
+			return (line);
 		if (ft_back(line) == 0)
 		{
 			n = ft_read(fd, buffer);
@@ -47,6 +46,7 @@ char *get_next_line(int fd)
 }
 /*
 #include <stdio.h>
+#include <string.h>
 
 int    main(void)
 {
@@ -54,9 +54,13 @@ int    main(void)
     //int        i;
     //char    rsl[9] = "wsh pelo";
 	char	*tab;
+	char *nul = NULL;
 
+	*/
+	//fd = open("files/41_no_nl", O_RDONLY);*/
+/*
     i = 0;
-    fd = open("files/empty", O_RDONLY);
+    
     while (i < 6)
     {
         tab = get_next_line(fd);
@@ -68,12 +72,67 @@ int    main(void)
         free(tab);
     }
     close(fd);
-
-	fd = open("files/41_no_nl", O_RDWR);
+*//*
+	fd = open("files/empty", O_RDWR);
 	tab = get_next_line(fd);
-			printf("%s", tab);
+	if (strcmp(tab, "01234567890123456789012345678901234567890\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "987654321098765432109876543210987654321090\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "0123456789012345678901234567890123456789012\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "987654321098765432109876543210987654321090\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	if (strcmp(tab, "01234567890123456789012345678901234567890\n") != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+			////////////////////////////////////////////////////////////////
+	tab = get_next_line(fd);
+	if (strcmp(tab, nul) != 0)
+		printf("error");
+	printf("%s", tab);
+	free(tab);
+}
+			tab = get_next_line(fd);
+	if (strcmp(tab, "01234567890123456789012345678901234567890") != 0)
+		printf("error");
+		 	printf("%s", tab);
 			free(tab);
-	tab = get_next_line(fd);
+			tab = get_next_line(fd);
+
 		 	printf("%s", tab);
 			free(tab);
 	close(fd);
