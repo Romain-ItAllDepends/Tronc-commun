8e360aebf88592267308c59f919939664b54932f
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-01 18:09:14 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line.h
+++ GET_NEXT_LINE/get_next_line.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:26:51 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/28 16:55:37 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/01 17:38:36 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -20,7 +20,7 @@
 #endif
 
 char	*get_next_line(int fd);
-char	*ft_split(char *stash, char *buffer, int count);
+char	*ft_split(char *stash, char *buffer);
 char	*ft_fill(char *line, char *buffer, char *tab);
 void	ft_clear(char *s, char *buffer);
 int	ft_strlen(char *s);
