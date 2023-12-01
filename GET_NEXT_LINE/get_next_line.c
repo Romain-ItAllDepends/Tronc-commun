c53efc94906830aee05cff0e401382d3a77e4e8b
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-01 18:08:23 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line.c
+++ GET_NEXT_LINE/get_next_line.c
@@ -1,21 +1,21 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:25:49 by rgobet            #+#    #+#             */
-/*   Updated: 2023/11/28 16:55:32 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/01 18:07:38 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "get_next_line.h"
 
 static char	*ft_join(char *s, char *b, int count)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	*tab;
 
 	i = 0;
@@ -32,18 +32,14 @@ static char	*ft_join(char *s, char *b, int count)
 		}
 	}
 	while (b[j])
-	{
-		tab[i] = b[j];
-		i++;
-		j++;
-	}
+		tab[i++] = b[j++];
 	tab[i] = 0;
 	if (count > 0)
 		free(s);
 	return (tab);
 }
 
-static char *next_line(int fd, char *buffer, char *stash)
+static char	*next_line(int fd, char *buffer, char *stash)
 {
 	while (1)
 	{
@@ -64,56 +60,57 @@ static char *next_line(int fd, char *buffer, char *stash)
 	}
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*stash;
-	char	*new_line;
-	char	buffer[BUFFER_SIZE + 1];
+	char		*new_line;
+	char		buffer[BUFFER_SIZE + 1];
 
 	if (fd == 0)
 		return (NULL);
-	new_line = NULL;
-	if (count > 0)
+	n = read(fd, buffer, BUFFER_SIZE);
+	buffer[n] = '\0';
+	if (n == 0 && (stash[0] == '\0' || !stash))
+		return (NULL);
+	else if (n == 0)
 	{
+		new_line = ft_split(line, buffer);
 		stash = NULL;
-		stash = ft_split(buffer, stash, count);
-	}
-	if (stash == NULL)
-	{
-		n = read(fd, buffer, BUFFER_SIZE);
-		buffer[n] = '\0';
-		if (n == 0)
-			return(NULL);
+		return (new_line);
 	}
 	else
 	{
 		if (stash)
 			stash = ft_split(&stash[ft_back(buffer) + 1], buffer, count);
 		else
+		{
 			stash = ft_split(stash, buffer, count);
-		//stash -> rempli 
-		//stash -> rempli delimitation \n
+			if (stash[0] == '\n' || ft_back(stash) > 0)
+				return (new_line = next_line(fd, buffer, stash));
+		}
 	}
 }
+
+//Je rempli buffer, je le met dans stash, je verif le \n si il y ai pas on re read puis split jusqu'au \n 
 
 #include <stdio.h>
 
 int	main(void)
 {
 	char	*tab;
-	int	fd;
-	int	i;
+	int		fd;
+	int		i;
 
 	i = 0;
 	fd = open("files/empty", O_RDWR);
-    while (i < 6)
-    {
-        tab = get_next_line(fd);
-        if (tab)
+	while (i < 6)
+	{
+		tab = get_next_line(fd);
+		if (tab)
 			printf("%s", tab);
-        i++;
-        free(tab);
-    }
-    close(fd);
+		i++;
+		free(tab);
+	}
+	close(fd);
 	return (0);
 }
