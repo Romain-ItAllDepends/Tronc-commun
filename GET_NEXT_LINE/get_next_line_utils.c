90bab1f80eadd1fb3c2156562f246f438d81fce1
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-04 16:03:01 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line_utils.c
+++ GET_NEXT_LINE/get_next_line_utils.c
@@ -5,79 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/27 14:28:48 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/01 17:38:54 by rgobet           ###   ########.fr       */
+/*   Created: 2023/12/02 10:57:18 by rgobet            #+#    #+#             */
+/*   Updated: 2023/12/04 16:01:41 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "get_next_line.h"
-// Add une verif /n en option
-int	ft_strlen(char *s)
+
+int	ft_strlen(char *s, int opt)
 {
 	int	i;
 
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-// Supp
-int	ft_back(char *s)
-{
-	int	i;
-
-	if (!s)
+	if (!s || s[0] == '\0')
 		return (0);
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == '\n')
+		if (opt == 1 && s[i] == '\n')
 			return (i);
 		i++;
 	}
-	return (0);
-}
-// Supp
-void	ft_clear(char *s, char *buffer)
-{
-	int	i;
-
-	i = 0;
-	if (s)
-		free(s);
-	s = malloc(ft_strlen(&buffer[ft_back(buffer)]) * sizeof(char));
-	if (s == 0)
-		return ;
-	while (s[i])
-	{
-		s[i] = 0;
-		i++;
-	}
-}
-
-char	*ft_fill(char *line, char *buffer, char *tab)
-{
-	int	i;
-
-	i = 0;
-	if (line)
-	{
-		while (line[i])
-		{
-			tab[i] = line[i];
-			i++;
-		}
-		i = 0;
-	}
-	while (buffer[i] != '\n' && buffer[i] != '\0')
-	{
-		tab[i + ft_strlen(line)] = buffer[i];
-		i++;
-	}
-	tab[i + ft_strlen(line)] = '\0';
-	return (tab);
+	return (i);
 }
 
 char	*ft_split(char *stash, char *buffer)
@@ -85,14 +33,76 @@ char	*ft_split(char *stash, char *buffer)
 	char	*tab;
 	int		i;
 
-	i = (ft_strlen(stash) + ft_back(buffer)) + 1;
+	i = (ft_strlen(stash, 0) + ft_strlen(buffer, 1)) + 2;
 	tab = malloc(i * sizeof(char));
 	if (tab == 0)
 		return (0);
-	tab = ft_fill(stash, buffer, tab);
-	if (stash != 0)
+	i = 0;
+	if (stash)
+	{
+		while (stash[i++])
+			tab[i - 1] = stash[i - 1];
+	}
+	i--;
+	while (buffer[i - ft_strlen(buffer, 2)])
+	{
+		tab[i] = buffer[i - ft_strlen(stash, 0)];
+		i++;
+	}
+	tab[i] = '\0';
+	if ((stash || buffer[0] == '\0') && ft_strlen(stash, 1) == 0)
 		free(stash);
+	if (stash && ft_strlen(stash, 1) == 0)
+		stash = NULL;
 	return (tab);
 }
 
+char	*ft_join(char *s, char *b)
+{
+	int		i;
+	int		j;
+	char	*tab;
+
+	i = 0;
+	j = 0;
+	if (s)
+		tab = malloc ((ft_strlen(s, 0) + ft_strlen(b, 0)) * sizeof(char) + 2);
+	else
+		tab = malloc ((ft_strlen(b, 0)) * sizeof(char) + 1);
+	if (tab == 0)
+		return (NULL);
+	if (s)
+	{
+		while (s[i++])
+			tab[i] = s[i];
+	}
+	while (b[j])
+	{
+		tab[i++] = b[j];
+		b[j++] = 0;
+	}
+	tab[i] = 0;
+	if (s || b[0] == '\0')
+		free(s);
+	if (s)
+		s = NULL;
+	return (tab);
+}
+
+int	ft_read(int fd, char *buffer, char *stash)
+{
+	int	n;
+
+	n = read(fd, buffer, BUFFER_SIZE);
+	buffer[BUFFER_SIZE] = 0;
+	if (n == -1 || (n == 0 && stash[0] == '\0'))
+		return (0);
+	else if (n == 0)
+	{
+		free(stash);
+		stash = NULL;
+		return (-8);
+	}
+	return (n);
+}
 
