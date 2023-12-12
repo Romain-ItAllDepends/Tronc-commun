b96f397e87ab742995bffbab3e7df0f943b05ab2
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-12 10:59:57 +0100

    Add files via upload

--- GET_NEXT_LINE/get_next_line.c
+++ GET_NEXT_LINE/get_next_line.c
@@ -1,16 +1,26 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rgobet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/02 10:56:32 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/11 10:57:54 by rgobet           ###   ########.fr       */
+/*   Created: 2023/12/11 17:00:43 by rgobet            #+#    #+#             */
+/*   Updated: 2023/12/11 17:01:06 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_finish(char *s)
+{
+	if (s[0] == 0)
+	{
+		free(s);
+		s = NULL;
+	}
+	return (s);
+}
 
 char	*get_next_line(int fd)
 {
@@ -20,45 +30,22 @@ char	*get_next_line(int fd)
 	int			n;
 
 	stash = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	n = BUFFER_SIZE;
 	if (count > 0)
 		stash = ft_strjoin(buffer, stash, count);
-	while (1)
+	while (ft_strchr(stash, '\n') == 0 && n != 0)
 	{
-		if (ft_strchr(stash, '\n') == 0)
-		{
-			n = read(fd, buffer, BUFFER_SIZE);
-			buffer[n] = 0;
-			if (n < 0)
-				return (NULL);
-			stash = ft_strjoin(buffer, stash, count++);
-		}
-		if (ft_strchr(stash, '\n') > 0)
-			return (cut(stash, buffer));
-		else if (n == 0)
-			return (finish(stash));
+		n = read(fd, buffer, BUFFER_SIZE);
+		buffer[n] = 0;
+		if (n < 0)
+			return (NULL);
+		stash = ft_strjoin(buffer, stash, count++);
 	}
+	if (ft_strchr(stash, '\n') > 0)
+		return (ft_cut(stash, buffer));
+	else if (n == 0)
+		return (ft_finish(stash));
 	return (0);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*tab;
-	int		fd;
-	int		i;
-
-	i = 0;
-	fd = open("files/nl", O_RDWR);
-	while (i <= 3)
-	{
-		tab = get_next_line(fd);
-		printf("%s", tab);
-		i++;
-		if (tab)
-			free(tab);
-	}
-	close(fd);
-	return (0);
-}
-*/
