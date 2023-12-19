793df15ee34a77f29054b17c28007fd26135963d
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-19 13:35:56 +0100

    Add files via upload

--- PUSH_SWAP/push_swap.c
+++ PUSH_SWAP/push_swap.c
@@ -1,15 +1,55 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:07:53 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/18 12:08:51 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/19 13:32:58 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "push_swap.h"
 
+int min(*p, int length)
+{
+	int	min;
+	int	index;
+	int	i;
 
+	i = 0;
+	min = tab[i];
+	while (i < length)
+	{
+		if (min > tab[i])
+		{
+			min = tab[i];
+			index = i;
+		}
+		i++;
+	}
+	return (index);
+}
+
+int	*sort(int *pa, int length)
+{
+	int	min;
+
+	min = min(pa, length);
+	while (min != 0)
+	{
+		
+	}
+}
+
+int	main(int ac, char **av)
+{
+	char	*full_char;
+	int		*tab;
+	int		length;
+
+	full_char = ft_strjoin(av, ac);
+	tab = conversion_char_to_int_array(full_char, ' ', &length);
+	return (0);
+}
