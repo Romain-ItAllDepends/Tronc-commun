65413762e319e3260ec00759bb6b5893167178bd
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-20 11:07:34 +0100

    Add files via upload

--- PUSH_SWAP/instruct_two.c
+++ PUSH_SWAP/instruct_two.c
@@ -1,22 +1,21 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   instruct_two.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 14:14:09 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/18 14:45:23 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/20 11:02:57 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
-#include "../push_swap.h"
+#include "push_swap.h"
 
 int	*ft_reverse_rotate(int *tab, int length, char c)
 {
 	//Décale d’une position vers le bas tous les élements de la pile a ou b selon les params.
 	//Le dernier élément devient le premier.
-	int	temp;
 	int	*rr;
 	int	i;
 
@@ -30,7 +29,7 @@ int	*ft_reverse_rotate(int *tab, int length, char c)
 		i++;
 	}
 	rr[0] = tab[length - 1];
-	if (!c == x)
+	if (c != 'x')
 		ft_printf("rr%c", c);
 	free(tab);
 	return (tab);
@@ -38,7 +37,7 @@ int	*ft_reverse_rotate(int *tab, int length, char c)
 
 void	rrr(int **pa, int **pb, int la, int lb)
 {
-	*pa = ft_reverse_rotate(*pa, length, 'r');
-	*pb = ft_reverse_rotate(*pb, length, 'x');
+	*pa = ft_reverse_rotate(*pa, la, 'r');
+	*pb = ft_reverse_rotate(*pb, lb, 'x');
 	return ;
 }
