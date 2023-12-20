878bd2c6b748d09aea13db404aa4472f0d53b0bc
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-20 11:09:30 +0100

    Add files via upload

--- PUSH_SWAP/push_swap.h
+++ PUSH_SWAP/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:08:56 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/19 13:01:55 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/20 11:01:27 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,12 +17,14 @@
 # include "FT_PRINTF/LIBFT/libft.h"
 
 int		*ft_swap(int *tab, char c);
-void	ss(int **pa, int **pb, int la, int lb);
-int		*ft_push(int *tab, int x, int length, char c);
+void	ss(int **pa, int **pb);
+void	ft_push(int **pa, int **pb, int *length, char c);
 int		*ft_rotate(int *tab, int length, char c);
 void	rr(int **pa, int **pb, int la, int lb);
 int		*ft_reverse_rotate(int *tab, int length, char c);
 void	rrr(int **pa, int **pb, int la, int lb);
 int		*conversion_char_to_int_array(char const *s, char c, int *length);
+int		*ft_split_init(int **pa, int *length, char c);
+char	*ft_strjoin(char **s, int nbList);
 
 #endif
