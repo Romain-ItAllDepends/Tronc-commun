d1f2fe0aafb1e9c66475d9f8b39bb9cea8b06325
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-21 15:57:35 +0100

    Add files via upload

--- PUSH_SWAP/push_swap_utils.c
+++ PUSH_SWAP/push_swap_utils.c
@@ -6,13 +6,13 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 14:19:40 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/20 13:44:18 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/21 14:24:00 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "push_swap.h"
 
-int	*ft_split_init(int **pa, int *lengtha, int *lengthb)
+int	*ft_split_initb(int **pa, int **pb, int *lengtha, int *lengthb)
 {
 	int	*pb;
 	int	i;
@@ -21,9 +21,61 @@ int	*ft_split_init(int **pa, int *lengtha, int *lengthb)
 	pb = NULL;
 	while (i < (*lengtha / 2))
 	{
-		pb = ft_push(pb, lengthb, *pa[0], 'b');
-		*pa = ft_push_balance(*pa, lengtha);
+		if (*pa[i] > *pa[*lengtha / 2])
+		{
+			pb = ft_push(pb, lengthb, *pa[0], 'b');
+			*pa = ft_push_balance(*pa, lengtha);
+		}
+		else
+			*pa = ft_rotate(pa, lengtha, 'a');
 		i++;
 	}
 	return ((int *)pb);
+}
+
+int	*ft_split_inita(int **pb, int **pa, int *lengtha, int *lengthb)
+{
+	int	i;
+
+	i = 0;
+	while (i < (*lengthb / 2))
+	{
+		if (*pb[i] < *pb[*lengthb / 2])
+		{
+			*pa = ft_push(pb, lengthb, *pa[0], 'b');
+			*pb = ft_push_balance(*pa, lengtha);
+		}
+		else
+			*pb = ft_rotate(pb, lengthb, 'b');
+		i++;
+	}
+	return ((int *)pa);
+}
+
+int	*sort_bubble(int *tab, length)
+{
+	int	temp;
+	int	i;
+	int	j;
+	int	l;
+
+	i = 0;
+	l = length;
+	while (i < length)
+	{
+		j = 0;
+		while (j < l)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		l--;
+		i++;
+	}
+	return (tab);
 }
