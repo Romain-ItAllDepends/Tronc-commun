71de38007ffd07bb23146cb517dbb157b00e800b
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2023-12-29 11:14:08 +0100

    Add files via upload

--- PUSH_SWAP/push_swap.c
+++ PUSH_SWAP/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:07:53 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/28 12:57:43 by rgobet           ###   ########.fr       */
+/*   Updated: 2023/12/29 11:12:00 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -35,37 +35,29 @@ int	*sort(int *pa, int length)
 int	*core(int *pa, int *lengtha)
 {
 	int	*pb;
-	int	*chunk;
+	int	**chunk;
 	int	lengthb;
+	int	lengthc;
 	int	i;
 
 	i = 0;
 	lengthb = 0;
+	lengthc = 0;
 	pb = NULL;
 	chunk = NULL;
 	while (*lengtha > 2 || decreasing(pa, *lengtha) == 1)
 	{
-	//Boucle infini ici condition a revoir
-		chunk = chunk_init(chunk, sort_bubble(pa, *lengtha));
+		lengthc++;
+		chunk = chunk_init(sort_bubble(pa, *lengtha), pa, chunk);
+		if (!chunk)
+			exit(1);
 		ft_split_initb(&pa, &pb, lengtha, &lengthb);
 		i++;
 	}
-	while (lengthb > 2 || decreasing(pb, lengthb) == 1)
+	while (lengthb != 0 || decreasing(pb, lengthb) == 1)
 		ft_split_inita(&pb, &pa, lengtha, &lengthb);
-	//pa = sort(pa, *lengtha);
-	//pb = sort(pb, lengthb);
-	// while (*lengtha > i)
-	// {
-	// 	ft_printf("pa %d\n", pa[i]);
-	// 	i++;
-	// }
-	// i = 0;
-	// 	while (lengthb > i)
-	// {
-	// 	ft_printf("pb %d\n", pb[i]);
-	// 	i++;
-	// }
 	free(pb);
+	ft_free(chunk, lengthc);
 	return (pa);
 }
 
