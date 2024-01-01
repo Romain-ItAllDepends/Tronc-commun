dff162b5af12793ebafa2b42619bf5a665ba8903
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-01 16:54:50 +0100

    Add files via upload

---
+++ PUSH_SWAP/ft_calloc.c
@@ -0,0 +1,42 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 13:47:07 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/01 14:22:17 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "push_swap.h"
+
+static void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*tab;
+
+	i = 0;
+	tab = s;
+	while (i < n)
+	{
+		tab[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned long	s;
+	void			*ptr;
+
+	s = nmemb * size;
+	if (s < 0 || ((int)nmemb < 0 && (int)size < 0))
+		return (NULL);
+	ptr = malloc(s);
+	if (ptr)
+		ft_memset(ptr, 0, s);
+	return (ptr);
+}
