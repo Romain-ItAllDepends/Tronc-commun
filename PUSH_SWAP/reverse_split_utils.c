60a2ec5f47144f83d71ede1f5f0fc5b0210c13b1
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-01 16:56:29 +0100

    Add files via upload

--- PUSH_SWAP/reverse_split_utils.c
+++ PUSH_SWAP/reverse_split_utils.c
@@ -6,26 +6,34 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 13:43:55 by rgobet            #+#    #+#             */
-/*   Updated: 2023/12/30 13:51:41 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/01 15:46:40 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "push_swap.h"
 
-void	ft_chunk_check(int **pa, int **pb, int ***chunk)
+/*
+* We will sort in a decreasing order each chunk starting by the chunk above
+* the stack b.
+*/
+
+void	ft_chunk_check(t_vars *vars)
 {
 	int			i;
 	int			j;
 	static int	index_midpoint = i + 1;
 
 	j = 0;
-	i = chunk[0][0] - 2;
+	i = vars->len_c - 2;
 	index_midpoint--;
 	while (i > 0)
 	{
-		while (j < chunk[chunk[0][0]][1])
+		while (vars->chunk[i][2] != vars->pb[j])
+			j++;
+		while (j < vars->len_b)
 		{
-
+			if (j)
+				j++;
 			j++;
 		}
 		i--;
