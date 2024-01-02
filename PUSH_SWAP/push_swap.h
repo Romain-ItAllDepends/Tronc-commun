7fb4a83ec854f1c3bb4eb0396a31301585d04a65
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-02 11:17:04 +0100

    Add files via upload

--- PUSH_SWAP/push_swap.h
+++ PUSH_SWAP/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:08:56 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/01 13:53:34 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/02 11:08:11 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -22,6 +22,7 @@ typedef struct s_vars{
 	int	len_b;
 	int	len_c;
 	int	nb_chunk;
+	int	**chunk;
 	int	*midpoint;
 	int	*pa;
 	int	*pb;
@@ -30,14 +31,14 @@ typedef struct s_vars{
 /*
 * Instructions of Push Swap
 */
-int		*ft_swap(int *tab, char c);
-void	ss(int **pa, int **pb);
-int		*ft_push(int *p, int *length, int n, char c);
-int		*ft_push_balance(int *p, int *length);
-int		*ft_rotate(int *tab, int length, char c);
-void	rr(int **pa, int **pb, int la, int lb);
-int		*ft_reverse_rotate(int *tab, int length, char c);
-void	rrr(int **pa, int **pb, int la, int lb);
+void	ft_swap(int *tab, char c);
+void	ss(t_vars *vars);
+void	ft_push(int *p, int *length, int n, char c);
+void	ft_push_balance(int *p, int *length);
+void	ft_rotate(int *tab, int length, char c);
+void	rr(t_vars *vars);
+void	ft_reverse_rotate(int *tab, int length, char c);
+void	rrr(t_vars *vars);
 
 /*
 * Parsing
@@ -48,16 +49,14 @@ char	*ft_strjoin(char **s, int nbList);
 /*
 * Sort
 */
-int		*ft_split_initb(int **pa, int **pb, int *lengtha, int *lengthb);
-int		*ft_split_inita(int **pb, int **pa, int *lengtha, int *lengthb);
+void	ft_split_initb(t_vars *vars);
 int		sort_bubble(int *tab, int length);
 int		ft_midpoint(int *tab, int length);
 int		decreasing(int *pb, int lengthb);
 int		*ft_switch(int *sort, int j);
 int		*ft_fill(int *tab, int length);
-int		**chunk_init(int newmidpoint, int *p, int **chunk);
-int		*prepare(int lengtha, int lengthb);
-void	reverse_split(int **pa, int **pb, int *lengtha, int *lengthb);
+int		**chunk_init(int newmidpoint, t_vars *vars);
+void	reverse_split(t_vars *vars);
 
 /*
 * Error
