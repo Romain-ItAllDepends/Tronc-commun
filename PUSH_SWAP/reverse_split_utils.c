a8655928210ae75f5e8a87c6cd216fe52ed2ddb3
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-03 11:36:06 +0100

    Add files via upload

--- PUSH_SWAP/reverse_split_utils.c
+++ PUSH_SWAP/reverse_split_utils.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 13:43:55 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/02 11:09:26 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/03 11:34:09 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -38,4 +38,29 @@ void	ft_chunk_check(t_vars *vars)
 		}
 		i--;
 	}
+}
+
+/*
+* Verification of a chunk.
+* If the chunk (midpoint) in settings contain only one number return 0.
+* Otherwise return 1.
+*/
+
+int	alone(int midpoint, t_vars *vars)
+{
+	int	i;
+	int	ic;
+	int	c;
+
+	i = 0;
+	while (ic < vars->len_c && vars->chunk[ic] != midpoint)
+		ic++;
+	while (i < vars->len_b && vars->pb[i] < vars->chunk[ic - 1])
+		i++;
+	j = i;
+	while (i < vars->len_b)
+		i++;
+	if (i - j != 1)
+		return (1);
+	return (0);
 }
