85dff693a6e809141fb8c23cdd190c8593fd11d8
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-03 11:35:57 +0100

    Add files via upload

--- PUSH_SWAP/reverse_split.c
+++ PUSH_SWAP/reverse_split.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 13:55:58 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/02 11:09:14 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/03 11:26:29 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,14 +17,11 @@ static void	finish(t_vars *vars)
 	int	j;
 
 	j = 0;
-	if (decreasing(vars->pb, vars->len_b) == 0)
+	while (j < vars->len_b)
 	{
-		while (j < vars->len_b)
-		{
-			ft_push(vars->pa, &vars->len_a, vars->pb[0], 'b');
-			ft_push_balance(vars->pb, &vars->len_b);
-			j++;
-		}
+		vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+		vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+		j++;
 	}
 	return ;
 }
@@ -44,34 +41,29 @@ static void	decrease_order(t_vars *vars)
 	return ;
 }
 
-void	sort(t_vars *vars)
-{
-	int	i;
-
-	i = 0;
-	while (i < vars->len_b)
-	{
-		if (vars->chunk[i][1] == vars->chunk[i][2])
-		{
-			ft_push(vars->pa, &vars->len_a, vars->pb[0], 'b');
-			ft_push_balance(vars->pb, &vars->len_b);
-		}
-		else if (decreasing(vars->pb, vars->len_b) == 1)
-			decrease_order(vars);
-		else
-		{
-			finish(vars);
-			break ;
-		}
-		i++;
-	}
-}
-
 /*
 * Use the midpoint to sort the b stack and push it back in the stack a.
 */
 
 void	reverse_split(t_vars *vars)
 {
-	sort(vars);
+	int	i;
+
+	i = 0;
+	while (i < vars->len_b)
+	{
+		if (alone(vars->chunk[vars->len_c], vars) != 0)
+		{
+			ft_push(vars->pa, &vars->len_a, vars->pb[0], 'b');
+			ft_push_balance(vars->pb, &vars->len_b);
+		}
+		else if (decreasing(vars->pb, vars->len_b) == 1)
+			decrease_order(vars);
+		else if (decreasing(vars->pb, vars->len_b) == 0)
+		{
+			finish(vars);
+			break ;
+		}
+		i++;
+	}
 }
