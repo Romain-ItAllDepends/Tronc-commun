6def5f24bffcde200d16de4328d5377170934c1c
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-06 14:32:52 +0100

    Add files via upload

--- PUSH_SWAP/reverse_split.c
+++ PUSH_SWAP/reverse_split.c
@@ -6,18 +6,22 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 13:55:58 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/04 16:22:00 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/06 13:55:11 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "push_swap.h"
 
+void	alone_push(t_vars *vars);
+
 static void	finish(t_vars *vars)
 {
 	int	j;
+	int	length;
 
 	j = 0;
-	while (j < vars->len_b)
+	length = vars->len_b;
+	while (j < length)
 	{
 		vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
 		vars->pb = ft_push_balance(vars->pb, &vars->len_b);
@@ -47,32 +51,41 @@ static void	decrease_order(t_vars *vars)
 
 /*
 * Use the midpoint to sort the b stack and push it back in the stack a.
+* I think it's a better idea to while on the length of the chunk.
 */
 
 void	reverse_split(t_vars *vars)
 {
 	int	i;
+	int	length;
 
 	i = 0;
-	while (i < vars->len_b)
+	length = vars->len_b;
+	while (i < length)
 	{
-		if (alone(vars->chunk[vars->len_c], vars) != 0)
-		{
-			vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'b');
-			vars->pb = ft_push_balance(vars->pb, &vars->len_b);
-			i++;
-		}
-		else if (decreasing(vars->pb, vars->len_b) == 1)
-		{
+		if (alone(vars->chunk[vars->len_c - 1], vars) != 0)
+			alone_push(vars);
+		else if (decreasing(vars->pb, vars->len_b) == 1 && i != vars->len_c)
 			ft_chunk_check(vars, i);
-			i++;
-		}
 		else if (decreasing(vars->pb, vars->len_b) == 0)
 		{
 			finish(vars);
 			break ;
 		}
-		if (i == vars->len_c)
+		else if (i == vars->len_c)
 			special_case(vars);
+		if (crescent(vars->pa, vars->len_a) == 1)
+			order_a(vars); // reverse(); maybe
+		i++;
 	}
+}
+
+void	alone_push(t_vars *vars)
+{
+	if (vars->len_a > 1 && vars->pa[0] > vars->pa[1])
+		ft_swap(vars->pa, 'a');
+	if (vars->len_b > 1 && vars->pb[0] < vars->pb[1])
+		ft_swap(vars->pb, 'b');
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'b');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
 }
