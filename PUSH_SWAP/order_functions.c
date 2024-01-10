a727fcc71e7b35588aceb2e904a733b003b36fc6
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-10 16:17:23 +0100

    Add files via upload

--- PUSH_SWAP/order_functions.c
+++ PUSH_SWAP/order_functions.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 14:31:52 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/06 13:12:41 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/10 13:00:47 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -21,18 +21,20 @@ int	decreasing(int *pb, int lengthb)
 	int	j;
 	int	l;
 
-	i = -1;
+	i = 0;
 	l = lengthb;
 	sort = ft_fill(pb, lengthb);
-	while (i++ < lengthb - 1)
+	while (i < lengthb - 1)
 	{
-		j = -1;
-		while (j++ < l - 2)
+		j = 0;
+		while (j < l - 2)
 		{
 			if (sort[j] < sort[j + 1])
 				sort = ft_switch(sort, j);
+			j++;
 		}
 		l--;
+		i++;
 	}
 	i = ft_compare(pb, sort, lengthb);
 	free(sort);
