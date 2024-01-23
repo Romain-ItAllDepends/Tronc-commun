fe2b9e55a2b7cddef9d744683c55b55fdf4af233
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-23 13:49:57 +0100

    Add files via upload

--- PUSH_SWAP/push_swap.c
+++ PUSH_SWAP/push_swap.c
@@ -1,38 +1,17 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 12:07:53 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/13 16:52:30 by rgobet           ###   ########.fr       */
+/*   Created: 2024/01/23 13:24:10 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/23 13:25:06 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*
-int	*sort(int *pa, int length)
-{
-	int	i;
-
-	i = 0;
-	while (i < length)
-	{
-		if (pa[i] < min)
-		{
-			min = pa[i];
-			while (pa[0] != min)
-				pa = ft_reverse_rotate(pa, length, 'a');
-		}
-		i++;
-	}
-	return (pa);                                                                                   
-}
-*/
-// while (lengthb != 0 || decreasing(pb, lengthb) == 1)
-// 	ft_split_inita(&pb, &pa, lengtha, &lengthb);
 t_vars	*core(t_vars *vars)
 {
 	int	i;
@@ -62,40 +41,78 @@ t_vars	*core(t_vars *vars)
 	return (vars);
 }
 
-/*
-* The instruct_one.c and instruct_two.c are verified !
-* Actually making the algorithm.
-*/
+void	ft_three(t_vars *vars)
+{
+	vars->np = normalisation(vars->pa, vars->len_a);
+	if (vars->np[0] == 1 && vars->np[1] == 0)
+		ft_swap(vars->pa, 'a');
+	else if (vars->np[0] == 2 && vars->np[1] == 1)
+	{
+		vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+		ft_swap(vars->pa, 'a');
+	}
+	else if (vars->np[0] == 2 && vars->np[1] == 0)
+		vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+	else if (vars->np[0] == 0 && vars->np[1] == 2)
+	{
+		ft_swap(vars->pa, 'a');
+		vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+	}
+	else if (vars->np[0] == 1 && vars->np[1] == 2)
+		vars->pa = ft_reverse_rotate(vars->pa, vars->len_a, 'a');
+	free(vars->np);
+}
+
+void	ft_five(t_vars *vars)
+{
+	while (vars->pa[0] != nb_min(vars->pa, vars->len_a))
+		vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	while (vars->pa[0] != nb_max(vars->pa, vars->len_a))
+		vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	ft_three(vars);
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	vars->pa = ft_rotate(vars->pa, vars->len_a, 'a');
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	free(vars->pb);
+}
+
+t_vars	*ft_sort_choice(t_vars *vars)
+{
+	if (vars->len_a == 2)
+		ft_swap(vars->pa, 'a');
+	if (vars->len_a == 3)
+		ft_three(vars);
+	if (vars->len_a == 4)
+		ft_four(vars);
+	if (vars->len_a == 5)
+		ft_five(vars);
+	else
+		vars = core(vars);
+	return (vars);
+}
 
 int	main(int ac, char **av)
 {
 	t_vars	*vars;
 	char	*full_char;
-	int		i;
 
 	if (ac == 1)
 		exit(1);
-	i = 0;
 	vars = ft_calloc(sizeof(t_vars), 1);
 	if (!vars)
 		exit(1);
-	full_char = ft_strjoin(av, ac);
-	vars->pa = conversion_char_to_int_array(full_char, ' ', &vars->len_a);
+	full_char = ft_strjoin(av, ac, vars);
+	vars->pa = conversion_char_to_int_array(full_char, ' ', &vars->len_a, vars);
 	free(full_char);
-	error_duplication(vars->pa, vars->len_a);
-	if (!(crescent(vars->pa, vars->len_a) == 0))
-		vars = core(vars);
-	while (i < vars->len_a)
-	{
-		ft_printf("\n%d", vars->pa[i]);
-		i++;
-	}
-	// ft_printf("\n\n%d", vars->len_a);
-	// if (!(crescent(vars->pa, vars->len_a) == 0))
-	// {
-	// 	ft_swap(vars->pa, 'a');
-	// 	vars->pa = ft_reverse_rotate(vars->pa, vars->len_a, 'a');
-	// }
+	error_duplication(vars->pa, vars->len_a, vars);
+	if (crescent(vars->pa, vars->len_a) == 1)
+		vars = ft_sort_choice(vars);
 	free(vars->pa);
 	free(vars);
 	return (0);
