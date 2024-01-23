5ce2d3b807928bfe1beae6d1c189558d4172ddd6
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-23 14:22:40 +0100

    Add files via upload

--- PUSH_SWAP/order_functions.c
+++ PUSH_SWAP/order_functions.c
@@ -1,18 +1,30 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   order_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/26 14:31:52 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/10 13:00:47 by rgobet           ###   ########.fr       */
+/*   Created: 2024/01/23 13:24:10 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/23 13:25:06 by rgobet           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_compare(int *pb, int *sort, int length);
+static int	ft_compare(int *pb, int *sort, int length)
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		if (pb[i] != sort[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int	decreasing(int *pb, int lengthb)
 {
@@ -27,13 +39,12 @@ int	decreasing(int *pb, int lengthb)
 	while (i < lengthb - 1)
 	{
 		j = 0;
-		while (j < l - 2)
+		while (j < l - 1)
 		{
 			if (sort[j] < sort[j + 1])
 				sort = ft_switch(sort, j);
 			j++;
 		}
-		l--;
 		i++;
 	}
 	i = ft_compare(pb, sort, lengthb);
@@ -68,16 +79,19 @@ int	crescent(int *pb, int lengthb)
 	return (i);
 }
 
-static int	ft_compare(int *pb, int *sort, int length)
+void	ft_four(t_vars *vars)
 {
-	int	i;
-
-	i = 0;
-	while (i < length)
-	{
-		if (pb[i] != sort[i])
-			return (1);
-		i++;
-	}
-	return (0);
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	if (vars->pb[0] < vars->pb[1])
+		ft_swap(vars->pb, 'b');
+	if (vars->pa[0] > vars->pa[1])
+		ft_swap(vars->pa, 'a');
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	free(vars->pb);
 }
