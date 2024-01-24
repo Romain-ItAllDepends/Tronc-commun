509f952fe6b2d0f840823044a6729d257f8cf703
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-24 12:41:11 +0100

    Add files via upload

--- PUSH_SWAP/chunk_functions.c
+++ PUSH_SWAP/chunk_functions.c
@@ -37,3 +37,49 @@ int	*chunk_init(int newmidpoint, t_vars *vars)
 		free(vars->chunk);
 	return (newchunk);
 }
+
+/*
+* We will sort in a decreasing order each chunk starting by the chunk above
+* the stack b.
+* We will sort all the chunk other than the last one.
+*/
+
+void	ft_chunk_check(t_vars *vars, int ind)
+{
+	int	i;
+	int	max;
+
+	max = max_chunk(vars, ind);
+	i = min_chunk(vars, ind);
+	if (i - max <= 2 && vars->pb[0] < vars->pb[1])
+		ft_swap(vars->pb, 'b');
+	while (i < max)
+	{
+		if (vars->pb[0] > vars->pb[1])
+		{
+			vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+			vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+			order_a(vars);
+			i++;
+		}
+		else if (vars->pb[0] < vars->pb[1])
+			ft_swap(vars->pb, 'b');
+	}
+}
+
+int	select_chunk_size(int *sort, int length)
+{
+	int	mid;
+
+	mid = 0;
+	if (length < 53)
+		mid = sort[length / 2];
+	else if (length < 103)
+		mid = sort[length / 4];
+	else if (length < 253)
+		mid = sort[length / 10];
+	else if (length > 252)
+		mid = sort[length / 20];
+	free(sort);
+	return (mid);
+}
