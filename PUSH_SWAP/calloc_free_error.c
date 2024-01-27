e03c2c14c70d7e308efd229133322df2672bdf2e
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-27 14:24:34 +0100

    Add files via upload

---
+++ PUSH_SWAP/calloc_free_error.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc_free_error.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 13:24:10 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/23 13:25:06 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*tab;
+
+	i = 0;
+	tab = s;
+	while (i < n)
+	{
+		tab[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	long	s;
+	void	*ptr;
+
+	s = nmemb * size;
+	if (s < 0 || ((int)nmemb < 0 && (int)size < 0))
+		return (NULL);
+	ptr = malloc(s);
+	if (ptr)
+		ft_memset(ptr, 0, s);
+	return (ptr);
+}
+
+void	ft_error_zero(t_vars *vars)
+{
+	write (2, "Error\n", 6);
+	free(vars);
+	exit(1);
+}
