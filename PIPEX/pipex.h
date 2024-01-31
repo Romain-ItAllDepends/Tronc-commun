fbe54ab39318d786a896dd46d9dde2b526de1d6a
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-31 14:55:31 +0100

    Add files via upload

--- PIPEX/pipex.h
+++ PIPEX/pipex.h
@@ -6,13 +6,14 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 14:23:46 by rgobet            #+#    #+#             */
-/*   Updated: 2024/01/30 08:42:41 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/01/31 14:41:47 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include <fcntl.h>
+# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/wait.h>
@@ -21,17 +22,26 @@
 # include "FT_PRINTF/LIBFT/libft.h"
 
 typedef struct s_vars{
+	int		fd_parent;
+	int		fd_child;
+	int		fd[2];
 	char	*path1;
 	char	*path2;
 	char	*file1;
 	char	*file2;
 	char	**cmd1;
 	char	**cmd2;
+	pid_t	pid;
 }	t_vars;
 
 void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_free(char **s);
+void	verification(int ac, char **av);
 char	**ft_split(char const *s, char c);
+void	init_path(char **envp, t_vars *vars);
 char	*ft_fill(char *s, char **path, t_vars *vars);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*path_verification(char **path, t_vars *vars, int opt);
+char	*execution_right(char *cmd, char **path, t_vars *vars);
 
 #endif
