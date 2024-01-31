e4042f77d953e0009ee69ef21579aef96d6ccc80
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-01-31 14:56:19 +0100

    Add files via upload

---
+++ PIPEX/calloc_free.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 13:24:10 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/31 14:34:06 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*tab;
+
+	i = 0;
+	tab = s;
+	while (i < n)
+	{
+		tab[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned int	s;
+	void			*ptr;
+
+	s = nmemb * size;
+	if (s < 0 || ((int)nmemb < 0 && (int)size < 0))
+		return (NULL);
+	ptr = malloc(s);
+	if (ptr)
+		ft_memset(ptr, 0, s);
+	return (ptr);
+}
+
+void	ft_free(char **s)
+{
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+}
