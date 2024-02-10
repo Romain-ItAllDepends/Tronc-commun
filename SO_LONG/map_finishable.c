5fbe431383086d41cd43a5cb3e90511e21d598ec
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-10 14:51:16 +0100

    Add files via upload

--- SO_LONG/map_finishable.c
+++ SO_LONG/map_finishable.c
@@ -6,15 +6,18 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:22:13 by rgobet            #+#    #+#             */
-/*   Updated: 2024/02/02 18:08:14 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/02/09 11:22:14 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static char	**condition(char **map, int i, int j);
+
 static void	player_pos(char **map, int *i, int *j)
 {
 	*i = 0;
+	*j = 0;
 	while (map[*i])
 	{
 		*j = 0;
@@ -42,50 +45,49 @@ void	finishable(t_vars *vars)
 	while (map[i])
 	{
 		while (map[i][j])
+			map = condition(map, i, j++);
+		i++;
+		j = 0;
+	}
+	player_pos(map, &i, &j);
+	while (i >= 0 && map[i])
+	{
+		while (j >= 0 && map[i][j])
+			map = condition(map, i, j--);
+		j = ft_strlen(map[i--]) - 1;
+	}
+	i = 0;
+	while (map[i++])
+		map = security_finishable(map);
+	finishable_verif(vars, map);
+}
+
+char	**security_finishable(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		while (map[i][j])
 		{
-			if ((map[i][j] != '1' && map[i + 1][j] == 'V')
-			|| (map[i][j] != '1' && map[i - 1][j] == 'V')
-			|| (map[i][j] != '1' && map[i][j - 1] == 'V')
-			|| (map[i][j] != '1' && map[i][j + 1] == 'V')
-			|| (map[i][j] != '1' && map[i + 1][j] == 'P')
-			|| (map[i][j] != '1' && map[i - 1][j] == 'P')
-			|| (map[i][j] != '1' && map[i][j - 1] == 'P')
-			|| (map[i][j] != '1' && map[i][j + 1] == 'P'))
-				map[i][j] = 'V';
+			map = condition(map, i, j);
 			j++;
 		}
 		i++;
 		j = 0;
 	}
-	i = 0;
-	j = 0;
-	player_pos(map, &i, &j);
+	i = ft_strlen_mod(map) - 1;
+	j = ft_strlen(map[i]) - 1;
 	while (i >= 0 && map[i])
 	{
 		while (j >= 0 && map[i][j])
-		{
-			if ((map[i][j] != '1' && map[i + 1][j] == 'V')
-			|| (map[i][j] != '1' && map[i - 1][j] == 'V')
-			|| (map[i][j] != '1' && map[i][j - 1] == 'V')
-			|| (map[i][j] != '1' && map[i][j + 1] == 'V')
-			|| (map[i][j] != '1' && map[i + 1][j] == 'P')
-			|| (map[i][j] != '1' && map[i - 1][j] == 'P')
-			|| (map[i][j] != '1' && map[i][j - 1] == 'P')
-			|| (map[i][j] != '1' && map[i][j + 1] == 'P'))
-				map[i][j] = 'V';
-			j--;
-		}
-		j = ft_strlen(map[i]) - 1;
-		i--;
+			map = condition(map, i, j--);
+		j = ft_strlen(map[i--]) - 1;
 	}
-	finishable_verif(vars, map);
-	i = 0;
-	// while (map[i])
-	// {
-	// 	ft_printf("%s\n", map[i]);
-	// 	i++;
-	// }
-	ft_free(map);
+	return (map);
 }
 
 void	finishable_verif(t_vars *vars, char **map)
@@ -111,4 +113,19 @@ void	finishable_verif(t_vars *vars, char **map)
 		}
 		i++;
 	}
+	ft_free(map);
+}
+
+static char	**condition(char **map, int i, int j)
+{
+	if ((map[i][j] != '1' && map[i + 1][j] == 'V')
+			|| (map[i][j] != '1' && map[i - 1][j] == 'V')
+			|| (map[i][j] != '1' && map[i][j - 1] == 'V')
+			|| (map[i][j] != '1' && map[i][j + 1] == 'V')
+			|| (map[i][j] != '1' && map[i + 1][j] == 'P')
+			|| (map[i][j] != '1' && map[i - 1][j] == 'P')
+			|| (map[i][j] != '1' && map[i][j - 1] == 'P')
+			|| (map[i][j] != '1' && map[i][j + 1] == 'P'))
+		map[i][j] = 'V';
+	return (map);
 }
