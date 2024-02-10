ad1bfb7d5e0bd1b0bfd8c7ece12b06e3204261ab
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-10 15:01:54 +0100

    Add files via upload

---
+++ PUSH_SWAP/order_functions.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   order_functions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 13:24:10 by rgobet            #+#    #+#             */
+/*   Updated: 2024/01/27 14:20:03 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	ft_compare(int *pb, int *sort, int length)
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		if (pb[i] != sort[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	decreasing(int *pb, int lengthb)
+{
+	int	*sort;
+	int	i;
+	int	j;
+	int	l;
+
+	i = 0;
+	l = lengthb;
+	sort = ft_fill(pb, lengthb);
+	while (i < lengthb - 1)
+	{
+		j = 0;
+		while (j < l - 1)
+		{
+			if (sort[j] < sort[j + 1])
+				sort = ft_switch(sort, j);
+			j++;
+		}
+		i++;
+	}
+	i = ft_compare(pb, sort, lengthb);
+	free(sort);
+	return (i);
+}
+
+int	crescent(int *pb, int lengthb)
+{
+	int	*sort;
+	int	i;
+	int	j;
+	int	l;
+
+	i = 0;
+	l = lengthb;
+	sort = ft_fill(pb, lengthb);
+	while (i < lengthb)
+	{
+		j = 0;
+		while (j < l - 1)
+		{
+			if (sort[j] > sort[j + 1])
+				sort = ft_switch(sort, j);
+			j++;
+		}
+		l--;
+		i++;
+	}
+	i = ft_compare(pb, sort, lengthb);
+	free(sort);
+	return (i);
+}
+
+void	ft_four(t_vars *vars)
+{
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	vars->pb = ft_push(vars->pb, &vars->len_b, vars->pa[0], 'b');
+	vars->pa = ft_push_balance(vars->pa, &vars->len_a);
+	if (vars->pb[0] < vars->pb[1])
+		ft_swap(vars->pb, 'b');
+	if (vars->pa[0] > vars->pa[1])
+		ft_swap(vars->pa, 'a');
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	vars->pa = ft_push(vars->pa, &vars->len_a, vars->pb[0], 'a');
+	vars->pb = ft_push_balance(vars->pb, &vars->len_b);
+	free(vars->pb);
+}
+
+void	ss_or_sa(t_vars *vars)
+{
+	if (vars->pb[0] > vars->pb[1])
+		ft_swap(vars->pa, 'a');
+	else
+		ss(vars);
+}
