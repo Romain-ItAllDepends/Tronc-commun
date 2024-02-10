685454b7231c2ec9060d719e1adb6d82529cfc69
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-10 14:50:19 +0100

    Add files via upload

--- SO_LONG/so_long.c
+++ SO_LONG/so_long.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 08:55:58 by rgobet            #+#    #+#             */
-/*   Updated: 2024/02/03 03:44:05 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/02/10 12:56:49 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,24 @@ int	main(int ac, char *av[])
 {
 	t_vars	*vars;
 
-	vars = ft_calloc(sizeof(t_vars), 1);
+	vars = ft_calloc(sizeof(t_vars), 2);
 	if (!vars || ac != 2)
 	{
+		if (vars)
+			free(vars);
 		write (2, "Error\n", 6);
 		exit(1);
 	}
 	init_map(av[1], vars);
 	finishable(vars);
-	vars->mlx = mlx_init(ft_strlen(vars->map[0]) * 100, ft_strlen_mod(vars->map) * 100, "SO_LONG", false);
+	vars->mlx = mlx_init(ft_strlen(vars->map[0]) * 100,
+			ft_strlen_mod(vars->map) * 100, "SO_LONG", false);
 	vars->player_down_t = mlx_load_png("textures/player_down.png");
 	mlx_set_icon(vars->mlx, vars->player_down_t);
 	mlx_delete_texture(vars->player_down_t);
 	parse_map(vars, vars->mlx);
 	move(vars, vars->mlx);
 	mlx_loop(vars->mlx);
+	mlx_terminate(vars->mlx);
 	return (0);
 }
