c7bcdbb2f417a19bdb37fa6f5d8d222e44b4165a
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-16 17:34:11 +0100

    Add files via upload

---
+++ LIBFT/ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 11:09:52 by rgobet            #+#    #+#             */
+/*   Updated: 2024/02/16 11:22:40 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->next = NULL;
+	list->content = content;
+	return (list);
+}
