40b9104b9443b5a4842581356b61fa6c4d1865ca
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-16 10:42:19 +0100

    Add files via upload

---
+++ PHILOSOPHERS/philosophers_utils.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 15:24:25 by rgobet            #+#    #+#             */
+/*   Updated: 2024/02/11 15:30:29 by rgobet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	mult;
+	int	result;
+
+	i = 0;
+	mult = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-')
+	{
+		mult = -1;
+		i++;
+	}
+	else if (str[i] == '+' && mult == 1)
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * mult);
+}
