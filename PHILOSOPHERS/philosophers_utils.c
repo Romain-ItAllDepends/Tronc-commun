36ce13474e0fb5bf9030d01a474aa760c6bdad28
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-18 15:22:16 +0100

    Add files via upload

--- PHILOSOPHERS/philosophers_utils.c
+++ PHILOSOPHERS/philosophers_utils.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 15:24:25 by rgobet            #+#    #+#             */
-/*   Updated: 2024/02/11 15:30:29 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/02/18 14:50:26 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,55 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	return (result * mult);
+}
+
+t_philosopher	*ft_lstlast(t_philosopher *lst)
+{
+	while (lst && lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back_philo(t_philosopher **lst, t_philosopher *new)
+{
+	t_philosopher	*last;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
+
+static void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*tab;
+
+	i = 0;
+	tab = s;
+	while (i < n)
+	{
+		tab[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned int	s;
+	void			*ptr;
+
+	s = nmemb * size;
+	if (s < 0 || ((int)nmemb < 0 && (int)size < 0))
+		return (NULL);
+	ptr = malloc(s);
+	if (ptr)
+		ft_memset(ptr, 0, s);
+	return (ptr);
 }
