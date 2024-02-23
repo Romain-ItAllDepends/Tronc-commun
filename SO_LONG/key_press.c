3dc765818876f26406cee4006a837f62e575c9a9
Romain-ItAllDepends <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-02-23 17:26:14 +0100

    Add files via upload

--- SO_LONG/key_press.c
+++ SO_LONG/key_press.c
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 11:07:30 by rgobet            #+#    #+#             */
-/*   Updated: 2024/02/10 11:11:17 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/02/23 14:33:54 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,14 @@ void	down(t_vars *vars)
 	mlx_delete_image(vars->mlx, vars->player_forward_i);
 	mlx_delete_image(vars->mlx, vars->player_left_i);
 	mlx_delete_image(vars->mlx, vars->player_right_i);
+	vars->player_down_i = mlx_texture_to_image(vars->mlx, vars->player_down_t);
+	vars->player_forward_i = mlx_texture_to_image(vars->mlx,
+			vars->player_forward_t);
+	vars->player_right_i = mlx_texture_to_image(vars->mlx,
+			vars->player_right_t);
+	vars->player_left_i = mlx_texture_to_image(vars->mlx,
+			vars->player_left_t);
 	vars->i++;
-	vars->player_down_i = mlx_texture_to_image(vars->mlx,
-			vars->player_down_t);
 	mlx_image_to_window(vars->mlx, vars->player_down_i,
 		64 * vars->j, 64 * vars->i);
 	vars->nb_move++;
@@ -39,17 +44,12 @@ void	down(t_vars *vars)
 	if (vars->map[vars->i][vars->j] == 'C')
 	{
 		vars->map[vars->i][vars->j] = '0';
-		vars->grass_i = mlx_texture_to_image(vars->mlx, vars->grass_t);
-		mlx_image_to_window(vars->mlx, vars->grass_i,
-			64 * vars->j, 64 * vars->i);
-		vars->player_down_i = mlx_texture_to_image(vars->mlx,
-				vars->player_down_t);
 		mlx_image_to_window(vars->mlx, vars->player_down_i,
 			64 * vars->j, 64 * vars->i);
-		vars->nb_collectable--;
+		reprint_collectable(vars);
 	}
 	if (vars->map[vars->i][vars->j] == 'E' && vars->nb_collectable == 0)
-		ft_error_four(vars);
+		ft_win(vars);
 }
 
 void	up(t_vars *vars)
@@ -58,9 +58,14 @@ void	up(t_vars *vars)
 	mlx_delete_image(vars->mlx, vars->player_forward_i);
 	mlx_delete_image(vars->mlx, vars->player_left_i);
 	mlx_delete_image(vars->mlx, vars->player_right_i);
-	vars->i--;
+	vars->player_down_i = mlx_texture_to_image(vars->mlx, vars->player_down_t);
 	vars->player_forward_i = mlx_texture_to_image(vars->mlx,
 			vars->player_forward_t);
+	vars->player_right_i = mlx_texture_to_image(vars->mlx,
+			vars->player_right_t);
+	vars->player_left_i = mlx_texture_to_image(vars->mlx,
+			vars->player_left_t);
+	vars->i--;
 	mlx_image_to_window(vars->mlx, vars->player_forward_i,
 		64 * vars->j, 64 * vars->i);
 	vars->nb_move++;
@@ -68,17 +73,12 @@ void	up(t_vars *vars)
 	if (vars->map[vars->i][vars->j] == 'C')
 	{
 		vars->map[vars->i][vars->j] = '0';
-		vars->grass_i = mlx_texture_to_image(vars->mlx, vars->grass_t);
-		mlx_image_to_window(vars->mlx, vars->grass_i,
-			64 * vars->j, 64 * vars->i);
-		vars->player_forward_i = mlx_texture_to_image(vars->mlx,
-				vars->player_forward_t);
 		mlx_image_to_window(vars->mlx, vars->player_forward_i,
 			64 * vars->j, 64 * vars->i);
-		vars->nb_collectable--;
+		reprint_collectable(vars);
 	}
 	if (vars->map[vars->i][vars->j] == 'E' && vars->nb_collectable == 0)
-		ft_error_four(vars);
+		ft_win(vars);
 }
 
 void	left(t_vars *vars)
@@ -87,9 +87,14 @@ void	left(t_vars *vars)
 	mlx_delete_image(vars->mlx, vars->player_forward_i);
 	mlx_delete_image(vars->mlx, vars->player_left_i);
 	mlx_delete_image(vars->mlx, vars->player_right_i);
-	vars->j--;
+	vars->player_down_i = mlx_texture_to_image(vars->mlx, vars->player_down_t);
+	vars->player_forward_i = mlx_texture_to_image(vars->mlx,
+			vars->player_forward_t);
+	vars->player_right_i = mlx_texture_to_image(vars->mlx,
+			vars->player_right_t);
 	vars->player_left_i = mlx_texture_to_image(vars->mlx,
 			vars->player_left_t);
+	vars->j--;
 	mlx_image_to_window(vars->mlx, vars->player_left_i,
 		64 * vars->j, 64 * vars->i);
 	vars->nb_move++;
@@ -97,17 +102,12 @@ void	left(t_vars *vars)
 	if (vars->map[vars->i][vars->j] == 'C')
 	{
 		vars->map[vars->i][vars->j] = '0';
-		vars->grass_i = mlx_texture_to_image(vars->mlx, vars->grass_t);
-		mlx_image_to_window(vars->mlx, vars->grass_i,
-			64 * vars->j, 64 * vars->i);
-		vars->player_left_i = mlx_texture_to_image(vars->mlx,
-				vars->player_left_t);
 		mlx_image_to_window(vars->mlx, vars->player_left_i,
 			64 * vars->j, 64 * vars->i);
-		vars->nb_collectable--;
+		reprint_collectable(vars);
 	}
 	if (vars->map[vars->i][vars->j] == 'E' && vars->nb_collectable == 0)
-		ft_error_four(vars);
+		ft_win(vars);
 }
 
 void	right(t_vars *vars)
@@ -116,9 +116,14 @@ void	right(t_vars *vars)
 	mlx_delete_image(vars->mlx, vars->player_forward_i);
 	mlx_delete_image(vars->mlx, vars->player_left_i);
 	mlx_delete_image(vars->mlx, vars->player_right_i);
-	vars->j++;
+	vars->player_down_i = mlx_texture_to_image(vars->mlx, vars->player_down_t);
+	vars->player_forward_i = mlx_texture_to_image(vars->mlx,
+			vars->player_forward_t);
 	vars->player_right_i = mlx_texture_to_image(vars->mlx,
 			vars->player_right_t);
+	vars->player_left_i = mlx_texture_to_image(vars->mlx,
+			vars->player_left_t);
+	vars->j++;
 	mlx_image_to_window(vars->mlx, vars->player_right_i,
 		64 * vars->j, 64 * vars->i);
 	vars->nb_move++;
@@ -126,15 +131,10 @@ void	right(t_vars *vars)
 	if (vars->map[vars->i][vars->j] == 'C')
 	{
 		vars->map[vars->i][vars->j] = '0';
-		vars->grass_i = mlx_texture_to_image(vars->mlx, vars->grass_t);
-		mlx_image_to_window(vars->mlx, vars->grass_i,
-			64 * vars->j, 64 * vars->i);
-		vars->player_right_i = mlx_texture_to_image(vars->mlx,
-				vars->player_right_t);
 		mlx_image_to_window(vars->mlx, vars->player_right_i,
 			64 * vars->j, 64 * vars->i);
-		vars->nb_collectable--;
+		reprint_collectable(vars);
 	}
 	if (vars->map[vars->i][vars->j] == 'E' && vars->nb_collectable == 0)
-		ft_error_four(vars);
+		ft_win(vars);
 }
